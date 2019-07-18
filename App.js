ea5444bf200079160be9a750cbb4755b33f3c994
Alex Doss <alex.doss42@gmail.com>
2019-07-17 20:45:13 -0600

    Adds profile screen

--- App.js
+++ App.js
@@ -3,10 +3,11 @@ import React from 'react'
 import {createStackNavigator, createAppContainer} from 'react-navigation';
 
 import HomeScreen from './Components/HomeScreen'
+import ProfileScreen from './Components/ProfileScreen'
 
 const MainNavigator = createStackNavigator({
   Home: {screen: HomeScreen},
-  // Profile: {screen: ProfileScreen},
+  Profile: {screen: ProfileScreen},
 });
 
 const App = createAppContainer(MainNavigator);
